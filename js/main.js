10543e7480483dd313c1f1f568a30aee2f3f4642
wrap <wrap.escuadra@gmail.com>
2018-11-03 16:02:11 +0800

    init

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+
+	$('.js-wp-2').waypoint(function(direction) {
+
+		$('.js-wp-2').addClass('animated slideInUp');
+	
+	},{
+		offset: '70%'
+	});
+
+	$(".iphone-btn").delay(1300).animate( {bottom: "+=-3"}, 300);
+	$(".iphone-btn").delay(100).animate( {bottom: "+=3"}, 300);
+
+	$('.js-wp-3').waypoint(function(direction) {
+
+		$('.js-wp-3').addClass('animated fadeIn');
+	
+	},{
+		offset: '70%'
+	});
+
+
+
+
+
+
+
+
+
+}); //end jquery
