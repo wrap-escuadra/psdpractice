86373b88500405fb1be9482e299e32c79f592097
wrap <wrap.escuadra@gmail.com>
2018-11-04 09:20:19 +0800

    final

--- js/main.js
+++ js/main.js
@@ -1,28 +1,32 @@
 $(function(){
 	new WOW().init();
 
-	$('.burger-nav, .nav-close, .app-feat').on('click',function(){
+	$('.burger-nav, .nav-close').on('click',function(){
 		$('.container nav ul').toggleClass('open');
 	});
 
-	$('.js-wp-2').waypoint(function(direction) {
+	$('.navbar  ul li a').on('click', function(){
+		$('.container nav ul').removeClass('open');
+	});
+
+	// $('.js-wp-2').waypoint(function(direction) {
 
 		$('.js-wp-2').addClass('animated slideInUp');
 	
-	},{
-		offset: '70%'
-	});
+	// },{
+		// offset: '70%'
+	// });
 
 	$(".iphone-btn").delay(1300).animate( {bottom: "+=-3"}, 300);
 	$(".iphone-btn").delay(100).animate( {bottom: "+=3"}, 300);
 
-	$('.js-wp-3').waypoint(function(direction) {
+	// $('.js-wp-3').waypoint(function(direction) {
 
 		$('.js-wp-3').addClass('animated fadeIn');
 	
-	},{
-		offset: '70%'
-	});
+	// },{
+	// 	offset: '70%'
+	// });
 
 
 
